21ca3f549b448bda83cde8ec010f5d8e827b6ebe
Mariia Konovalchuk <71904680+MariiaKon@users.noreply.github.com>
2022-01-20 18:24:03 +0200

    task 8.2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import throttle from "lodash.throttle"
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(iframe);
+let currentTime;
+
+player.on('timeupdate', throttle(({ seconds }) => {
+    currentTime = seconds;
+    localStorage.setItem("videoplayer-current-time", currentTime);
+}, 1000));
+
+currentTime = localStorage.getItem("videoplayer-current-time");
+player.setCurrentTime(currentTime)
+    .then((currentTime) => {})
+    .catch((err) => {console.log('error');});
