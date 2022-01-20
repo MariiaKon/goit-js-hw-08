94aeae4ef82e3c95b43955b741843aee8f1a5e6e
Mariia Konovalchuk <71904680+MariiaKon@users.noreply.github.com>
2022-01-20 12:29:09 +0200

    task 8.1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
+import SimpleLightbox from "simplelightbox"
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems
+    .map(
+        ({ preview, original, description } = picture) => {
+            
+            const pictureMarkup = `<a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>`
+
+            return pictureMarkup
+        }
+    )
+    .join('');
+
+gallery.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+
+let openModal;
+
+gallery.addEventListener('click', openModal);
+
+openModal = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250, 
+});
