54097ed3e879276aa3a7db54296e3c0fbf623040
Mariia Konovalchuk <71904680+MariiaKon@users.noreply.github.com>
2022-01-20 20:56:10 +0200

    task 8.3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,58 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector('.feedback-form')
+let feedback = {
+    email: '',
+    message: ''
+};
+
+form.addEventListener('input', throttle(getFeedback, 500));
+form.addEventListener('submit', onFormSubmit);
+
+const savedData = localStorage.getItem("feedback-form-state");
+
+if (savedData) {
+    const parsedData = JSON.parse(savedData);
+    let { elements: { email, message } } = form;
+
+    email.value = parsedData.email;
+    message.textContent = parsedData.message;
+};
+
+function getFeedback() {
+    let { elements: { email, message } } = form;
+
+    feedback = {
+        email: email.value,
+        message: message.value
+    }
+
+    localStorage.setItem("feedback-form-state", JSON.stringify(feedback));
+};
+
+function onFormSubmit(e) {
+    e.preventDefault();
+
+    let { elements: { email, message } } = form;
+    console.log({ email: email.value, message: message.value });
+
+    formValidation();
+}
+
+function formValidation() { 
+    let { elements: { email, message } } = form;
+    if (email.value.trim() === '') {
+        alert("Please, fill in 'email' field")
+    };
+    if (message.value === '') {
+        alert("Please, fill in 'message' field")
+    };
+    if (email.value.trim() !== '' && message.value !== '') {
+        clearForm();
+    };
+}
+
+function clearForm() {
+    localStorage.clear();
+    form.reset();
+}
